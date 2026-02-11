8d6b4ea9a8f9830d982c592222384c51ba4ceace
basediyadeependra-collab <basediyadeependra@gmail.com>
2026-02-11 14:11:03 +0530

    Add files via upload

---
+++ Create_a_Personalized_Greeting.py
@@ -0,0 +1,8 @@
+"""
+Takes a user's first name and last name as input.
+concatenates the first name and last name into a full name.
+ print a personalized greeting message using the full name.
+"""
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello", first_name, last_name, "! welcome to the Python program.")
---
+++ Perform_Basic_Mathematics_operations.py
@@ -0,0 +1,15 @@
+"""
+when two numbers are - A and B
+performs the basic mathematics operations
+Addition = A + B
+Subtraction = A - B
+Multiplication = A * B
+Division = A/B
+"""
+
+A = float(input("Enter the first number: "))
+B = float(input("Enter the second number: "))
+print("Addition:" + str(A + B))
+print("Subtraction:" + str(A - B))
+print("Multiplication:" + str(A * B))
+print("Division:" + str(A / B))
